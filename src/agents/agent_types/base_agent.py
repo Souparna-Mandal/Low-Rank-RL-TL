848965751af5c78a774e10a8cccc22b959bbf189
Souparna Mandal <souparna2003@gmail.com>
2026-06-04 17:00:19 +0100

    DQN Agent added

--- src/agents/agent_types/base_agent.py
+++ src/agents/agent_types/base_agent.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
+import gymnasium as gym
 import pathlib
 import numpy as np
 import torch
 
 class BaseAgent(ABC):
+    def __init__(self, env: gym.Env):
+        self.agent_env = env
     @abstractmethod
     def pi(self, state: np.ndarray) -> np.ndarray | torch.tensor :
         """Policy pi: Returns an action or distribution over actions from env.action_space given the state.
@@ -26,3 +29,20 @@ class BaseAgent(ABC):
         """Loads respective state dicts from the given path and restores them to the agent
         instance. """
         raise NotImplementedError(f"{type(self).__name__} has not implemented load()")
+
+
+class EpsilonGreedyExplorer():
+    def __init__(self, eps_start, eps_min, decay_rate):
+        self.epsilon = eps_start
+        self.eps_min = eps_min
+        self.decay_rate = decay_rate
+        
+    def decay_epsilon(self):
+        # We follow an exponential decay scheme
+        self.epsilon = max(self.epsilon * self.decay_rate, self.eps_min)
+        
+    def get_epsilon(self):
+        return self.epsilon
+
+    def is_random_step(self):
+        return np.random.random() < self.epsilon
